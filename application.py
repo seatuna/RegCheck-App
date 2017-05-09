88622fefa0b20236f7d23603913ce81c63f8e071
Celena T <chibiynm@gmail.com>
2017-05-09 00:00:16 -0400

    Added get, post, patch, delete for venues, events

--- application.py
+++ application.py
@@ -1,55 +1,230 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 """ Python application for registration and check-in at events """
-from flask import Flask, request, jsonify, abort, json
-# from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-# from json import dumps
-# from flask.ext.jsonpify import jsonify
+from flask import Flask, request, json, abort
+from sqlalchemy import create_engine, exc
 
 # configure application
 APP = Flask(__name__)
-# api = Api(app)
 
 # connect db
 DB = create_engine("sqlite:///travcon.db")
 
 @APP.route("/entrants", methods=["GET", "POST", "PATCH", "DELETE"])
-def enter():
+def entrants():
     """ GET, POST, PATCH, and DELETE entrants for event registration """
+
+    # connect to DB and start transaction
     connection = DB.connect()
-    entrant_id = request.form.get("id")
-    name = request.form.get("name")
-    city = request.form.get("city")
-    state = request.form.get("state")
+    transaction = connection.begin()
 
-    # Returns a message with the request in JSON format
-    # if request.headers['Content-Type'] == 'application/json':
-    #     return "JSON Message: " + json.dumps(request.json)
+    # get data for POST / PATCH / DELETE requests
+    if request.json:
+        submitted_data = request.json["entrants"]
+        success_message = request.method + " successful!\n"
 
-    # POST Request
+    # POST Request. Create new entry, commit, and close connection if successful, rollback if error.
     if request.method == "POST":
-            # Return 400 error if no name on form
-        if not name:
+        name = submitted_data["name"]
+        city = submitted_data["city"]
+        state = submitted_data["state"]
+
+        try:
+            connection.execute("INSERT INTO entrants (name, city, state) \
+                VALUES (:name, :city, :state)", name=name, city=city, state=state)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
             abort(400)
+        finally:
+            connection.close()
 
-        connection.execute("INSERT INTO entrants (name, city, state) \
-            VALUES (:name, :city, :state)", name=name, city=city, state=state)
-        return 201
-
-    # PATCH Request
+    # PATCH Request. Update an entry, commit, and close connection if successful, rollback if error.
     elif request.method == "PATCH":
-        connection.execute("UPDATE entrants SET name = :name, city = :city, state = :state \
-                    WHERE id = :entrant_id", name=name, city=city, state=state,
-                           entrant_id=entrant_id)
-        return 201
+        entrant_id = submitted_data["id"]
+        name = submitted_data["name"]
+        city = submitted_data["city"]
+        state = submitted_data["state"]
 
-    # DELETE Request
+        try:
+            connection.execute("UPDATE entrants SET name = :name, city = :city, state = :state \
+                        WHERE id = :entrant_id", name=name, city=city, state=state,
+                               entrant_id=entrant_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # DELETE Request for entrants
     elif request.method == "DELETE":
-        connection.execute("DELETE FROM entrants WHERE id = :entrant_id", entrant_id=entrant_id)
+        entrant_id = submitted_data["id"]
 
-    # GET Request
+        try:
+            connection.execute("DELETE FROM entrants WHERE id = :entrant_id", entrant_id=entrant_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # GET Request for entrants
     else:
         query = connection.execute("SELECT * FROM entrants")
-        return json.dumps([dict(row) for row in query]), 201
+        return json.dumps({"entrants": [dict(row) for row in query]}), 201
 
-# @APP.route("/venues", methods=["GET", "POST", "PATCH", "DELETE"])
+@APP.route("/venues", methods=["GET", "POST", "PATCH", "DELETE"])
+def venues():
+    """ GET, POST, PATCH, and DELETE venues for event registration """
+
+     # connect to DB and start transaction
+    connection = DB.connect()
+    transaction = connection.begin()
+
+    # get data for POST / PATCH / DELETE requests
+    if request.json:
+        submitted_data = request.json["venues"]
+        success_message = request.method + " successful!\n"
+
+    # POST Request. Create new entry, commit, and close connection if successful, rollback if error.
+    if request.method == "POST":
+        name = submitted_data["name"]
+        address = submitted_data["address"]
+        city = submitted_data["city"]
+        state = submitted_data["state"]
+
+        try:
+            connection.execute("INSERT INTO venues (name, address, city, state) \
+                VALUES (:name, :address, :city, :state)", name=name, address=address,
+                               city=city, state=state)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # PATCH Request. Update entry, commit, and close connection if successful, rollback if error.
+    elif request.method == "PATCH":
+        venue_id = submitted_data["id"]
+        name = submitted_data["name"]
+        address = submitted_data["address"]
+        city = submitted_data["city"]
+        state = submitted_data["state"]
+
+        try:
+            connection.execute("UPDATE venues SET name = :name, address = :address, \
+            city = :city, state = :state WHERE id = :venue_id", name=name, address=address,
+                               city=city, state=state, venue_id=venue_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # DELETE Request. Delete entry, commit, and close connection if successful, rollback if error.
+    elif request.method == "DELETE":
+        venue_id = submitted_data["id"]
+
+        try:
+            connection.execute("DELETE FROM venues WHERE id = :venue_id", venue_id=venue_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # GET Request for venues
+    else:
+        query = connection.execute("SELECT * FROM venues")
+        return json.dumps({"venues": [dict(row) for row in query]}), 201
+
+@APP.route("/events", methods=["GET", "POST", "PATCH", "DELETE"])
+def events():
+    """ GET, POST, PATCH, and DELETE venues for event registration """
+
+    # connect to DB and start transaction
+    connection = DB.connect()
+    transaction = connection.begin()
+
+    # get data for POST / PATCH / DELETE requests
+    if request.json:
+        submitted_data = request.json["events"]
+        success_message = request.method + ' successful!\n'
+
+    # POST Request for events
+    if request.method == "POST":
+        name = submitted_data["name"]
+        description = submitted_data["description"]
+        games = submitted_data["games"]
+        venue_id = submitted_data["venue_id"]
+
+        try:
+            connection.execute("INSERT INTO events (name, description, games, venue_id) \
+                    VALUES (:name, :description, :games, :venue_id)", name=name,
+                               description=description, games=games, venue_id=venue_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # PATCH Request for events
+    elif request.method == "PATCH":
+        event_id = submitted_data["id"]
+        name = submitted_data["name"]
+        description = submitted_data["description"]
+        games = submitted_data["games"]
+        venue_id = submitted_data["venue_id"]
+
+        try:
+            connection.execute("UPDATE events SET name = :name, description = :description, \
+            games = :games, venue_id = :venue_id WHERE id = :event_id", name=name,
+                               description=description, games=games, venue_id=venue_id,
+                               event_id=event_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # DELETE Request for events
+    elif request.method == "DELETE":
+        event_id = submitted_data["id"]
+
+        try:
+            connection.execute("DELETE FROM events WHERE id = :event_id", event_id=event_id)
+            transaction.commit()
+            return success_message, 201
+        except exc.SQLAlchemyError as error:
+            transaction.rollback()
+            print(error)
+            abort(400)
+        finally:
+            connection.close()
+
+    # GET Request for events
+    else:
+        query = connection.execute("SELECT * FROM events")
+        return json.dumps({"events": [dict(row) for row in query]}), 201
